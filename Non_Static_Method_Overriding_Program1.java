ae28ac46273c8092f9893aa0296f5e30859f09a5
Avinash Kumar <60777365+avinashkumar1321@users.noreply.github.com>
2024-09-26 12:44:22 +0530

    method overriding with super keyword call assignment

---
+++ Non_Static_Method_Overriding_Program1.java
@@ -0,0 +1,31 @@
+package method.overriding;
+
+class Fackebook_email extends Fackebook_pass {
+
+	void Facebook() {
+		System.out.println("Facebook Email");
+		super.Facebook();
+
+	}
+
+}
+
+class Fackebook_pass {
+
+	void Facebook() {
+
+		System.out.println("Facebook Password");
+
+	}
+}
+
+public class Non_Static_Method_Overriding_Program1 {
+
+	public static void main(String[] args) {
+
+		Fackebook_email ob = new Fackebook_email();
+		ob.Facebook();
+
+	}
+
+}

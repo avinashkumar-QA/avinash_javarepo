8511647d8b8ee65e730e65845957b0604a4a09e3
“Avinash <avinashrajan2113@gmail.com>
2024-10-17 12:48:03 +0530

    While loop program

---
+++ Print_1_To_100_While_Loop.java
@@ -0,0 +1,18 @@
+package while_loop;
+
+public class Print_1_To_100_While_Loop {
+	
+	public static void main(String[] args) {
+		
+	int i = 1;
+	{
+
+		while (i <=100) {
+
+			System.out.println(i);
+			i++;
+		  }
+		}
+	}
+
+}
---
+++ While_Loop_Program_1.java
@@ -0,0 +1,24 @@
+package while_loop;
+
+public class While_Loop_Program_1 {
+	
+	//remember this if we don't know the iteration of cycle value go with while loop and if we know go with for loop.
+
+	public static void main(String[] args) {
+		
+		
+		int i= 5;
+		
+		while(i<=10) {
+			
+			System.out.println("1");
+			i++;
+			
+		}
+		
+		
+		
+	}
+	
+	
+}
